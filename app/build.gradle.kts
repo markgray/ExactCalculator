a69cd631e661beddfeefdbe2c6483c07a53195b8
markgray <mark.t.gray@gmail.com>
2025-04-25 06:18:53 -0400

    Updated AGP to 8.9.2 and updated gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 }
 
